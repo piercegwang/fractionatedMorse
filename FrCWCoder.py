906dd7eeeff63af946d3ab212ad2d75560eaf246
piercegwang <piercegwang@users.noreply.github.com>
2018-08-20 11:08:13 -0700

    Added cipher v1.0

---
+++ FrCWCoder.py
@@ -0,0 +1,119 @@
+import string
+import re
+
+morse_keys = {'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+     	'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+        
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.',
+        
+        '.': '.-.-.-', '=': '-...-',  '?': '..--..',
+        '!': '---.'
+        }
+
+rvmorse_keys = {v: k for k, v in morse_keys.items()}
+
+fractionated_table = [
+    '...', '..-', '.. ', '.-.', '.--', '.- ', '. .', '. -', '.  ',
+    '-..', '-.-', '-. ', '--.', '---', '-- ', '- .', '- -', '-  ',
+    ' ..', ' .-', ' . ', ' -.', ' --', ' - ', '  .', '  -'
+    ]
+
+
+letters = list(string.ascii_uppercase)
+
+def reduce_Keyword(keyword): #Reduces keyword - removes multiple letters
+    reduced = ""
+    for char in keyword:
+        if char.upper() not in reduced:
+            reduced+=char.upper()
+    return reduced
+
+def createAlphabet(redKeyword): #Creates alphabet using reduced keyword as input
+    alphabet = redKeyword
+    for char in letters:
+        if char not in alphabet:
+            alphabet += char
+    return alphabet
+
+def doMorse(input, do): #Converts message to morse code
+    if do == True:
+        morse = ""
+        for char in input:
+            if char != " ":
+                morse+=morse_keys[char.upper()] + " "
+            else:
+                morse+=" "
+        return morse
+    elif do == False:
+        input+=''
+        decipher = ''
+        citext = ''
+        i = 0
+        for dat in input:
+            if dat != " ":
+                i=0
+                citext += dat
+            else:
+                i += 1
+                if i == 2:
+                    decipher += ' '
+                else:
+                    decipher += rvmorse_keys[citext]
+                    citext = ''
+        return decipher
+            
+            
+
+def hashKey(alphabet): #makes a dictionary with keys being the keyword alphabet
+    new_alphabet = {}
+    for i in range(26):
+        new_alphabet[fractionated_table[i]] = alphabet[i]
+    return new_alphabet
+
+def fractionate(input, keyAlpha, do): #finally encoding/decoding
+    if do == True:
+        coded = ''
+        if(len(input)%3 != 0):
+            for i in range((len(input)%3)):
+                input+=" "
+        message = re.findall('...', input)
+        for i in message:
+            coded+=keyAlpha[i]
+        return coded
+    elif do == False:
+        decoded = ''
+        switched = {v: k for k, v in keyAlpha.items()}
+        for i in input:
+            decoded+=switched[i]
+        return decoded
+        
+        
+if __name__ == "__main__":
+    en_dec = input("Encoding or decoding? (Enter 1 | 2 respectively): ")
+    if en_dec == "1":
+        keyword = input("\nPlease enter your keyword: ")
+        keywordAlphabet = createAlphabet(reduce_Keyword(keyword))
+        keyAlphabet = hashKey(keywordAlphabet)
+        message = input("\nPlease paste message here: ")
+        print(fractionate(doMorse(message, True), keyAlphabet, True) + "\n")
+    elif en_dec == "2":
+        keyword = input("\nPlease enter your keyword: ")
+        keywordAlphabet = createAlphabet(reduce_Keyword(keyword))
+        print('Alphabet Key:' + keywordAlphabet)
+        keyAlphabet = hashKey(keywordAlphabet)
+        message = input("\nPlease enter coded message: ")
+        morse = fractionate(message, keyAlphabet, False)
+        print(morse)
+        print(doMorse(morse, False) + "\n")
+    
+    
